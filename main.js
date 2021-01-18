5ae9e0b1a53b4fc7e83229739b6bc7b5e6fea078
aderami <77590061+aderami@users.noreply.github.com>
2021-01-18 12:55:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 
 window.onload = () => {
-    gsap.to(".volet div", {
-        top: "-100%",
-        delay: 4,
-        stagger: 0.5
-    })
-    gsap.to("span", {
-        opacity: 0,
-        delay: 3.7
-    })
     anime({
         targets: '#KAIROS path',
         strokeDashoffset: [anime.setDashoffset, 0],
@@ -17,5 +8,18 @@ window.onload = () => {
         delay: function(el, i) { return i * 250},
         direction: 'alternate',
         loop: false
+    })
+    gsap.to("span", {
+        opacity: 0,
+        delay: 3.7
+    })
+    gsap.to(".volet div", {
+        top: "-100%",
+        delay: 4,
+        stagger: 0.5
+    })
+    gsap.to(".volet", {
+        display: "none",
+        delay: 5
     })
 }
